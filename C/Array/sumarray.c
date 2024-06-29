9c8dd00db8abfea1cbce1cc4912f15eb56293ed9
Mayaz120102 <abrarmayaz2002@gmail.com>
2024-06-29 14:47:23 +0600

    sum of array

---
+++ C/Array/sumarray.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int ar[5];
+  for(int i=0;i<=4;i++){
+    scanf("%d", &ar[i]);
+
+  }
+   for(int i=0;i<=4;i++){
+    printf("%d", ar[i]);
+  }
+  
+  int sum=0;
+   for(int i =0; i<5; i++){
+    sum =sum+ ar[i];
+   }
+
+  printf("%d\n",sum);
+
+}
