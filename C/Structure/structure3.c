c99ad1056c681b0945c9a9ca18622245da9c237b
Mayaz120102 <abrarmayaz2002@gmail.com>
2024-08-31 23:55:53 +0600

    array of structure

---
+++ C/Structure/structure3.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+
+struct Person
+{
+    char name[500];
+    int age;
+    float sal;
+
+};
+
+int main(){
+    struct Person  person[4];
+    int n;
+    printf("enter the number of person: ");
+    scanf("%d",&n);
+    getchar();
+    
+    for (int i = 0; i < n; i++)
+    {
+        printf("enter the info of person %d\n",i+1);
+        printf("enter the name: ");
+        fflush(stdin);
+        gets(person[i].name);
+        //fgets(person[i].name,sizeof(person[i].name),stdin);
+       // person[i].name[strcspn(person[i].name, "\n")] = '\0';
+        printf("enter age and sal: ");
+        scanf("%d %f",&person[i].age,&person[i].sal);
+        getchar();
+    }
+     
+     printf("\n");
+    
+    for(int i =0; i<n;i++)
+    {
+        printf("info of person: %d\n",i+1);
+        printf("Name: %s\n",person[i].name); 
+        printf("Age= %d Sal= %.2f\n",person[i].age,person[i].sal);
+    }
+
+}
